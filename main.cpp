37f159826af12693450b49948f074632129d1494
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-14 23:10:22 +0300

    Commit, Fixed minor syntex error

--- main.cpp
+++ main.cpp
@@ -16,17 +16,17 @@ int main()
     // Start by performing self test and reporting values
     myIMU.MPU9250SelfTest(myIMU.SelfTest);
     printf("x-axis self test: acceleration trim within : ");
-    printf(myIMU.SelfTest[0],1); printf("% of factory value\n");
+    printf("%d", myIMU.SelfTest[0]); printf("% of factory value\n");
     printf("y-axis self test: acceleration trim within : ");
-	printf(myIMU.SelfTest[1],1); print("% of factory value\n");
+	printf("%d", myIMU.SelfTest[1]); print("% of factory value\n");
 	printf("z-axis self test: acceleration trim within : ");
-	printf(myIMU.SelfTest[2],1); printf("% of factory value\n");
+	printf("%d", myIMU.SelfTest[2]); printf("% of factory value\n");
 	printf("x-axis self test: gyration trim within : ");
-	printf(myIMU.SelfTest[3],1); printf("% of factory value\n");
+	printf("%d", myIMU.SelfTest[3]); printf("% of factory value\n");
 	printf("y-axis self test: gyration trim within : ");
-	printf(myIMU.SelfTest[4],1); printf("% of factory value\n");
+	printf("%d", myIMU.SelfTest[4]); printf("% of factory value\n");
 	printf("z-axis self test: gyration trim within : ");
-	printf(myIMU.SelfTest[5],1); printf("% of factory value\n");	
+	printf("%d", myIMU.SelfTest[5]); printf("% of factory value\n");	
 
 	//Calibrate gyro and accelerometers, load biases in bias registers
     myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
