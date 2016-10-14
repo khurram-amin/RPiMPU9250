d956870e89a49391a7a4c2aebebee635f54fd332
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-15 00:54:23 +0300

    Commit, Fixed minor syntex error

--- PMPU9250_read.cpp
+++ PMPU9250_read.cpp
@@ -15,17 +15,17 @@ int main()
 //		cout<<"reading data ..."<<endl;
 		mpu9250.readAccelData(data);
 		cout<<"T: "<<mpu9250.readTempInC()<<endl;
-		for(int i=0; i<3; i++)
-		{
-			cout<<i<<": "<<1.0f*data[i]*mpu9250.getAres()<<endl;
-		}
-		cout<<endl;
-		mpu9250.readGyroData(data);
-		for(int i=0; i<3; i++)
-		{
-			cout<<i<<": "<<1.0f*data[i]*mpu9250.getGres()<<endl;
-		}
-		cout<<endl;
+		// for(int i=0; i<3; i++)
+		// {
+		// 	cout<<i<<": "<<1.0f*data[i]*mpu9250.getAres()<<endl;
+		// }
+		// cout<<endl;
+		// mpu9250.readGyroData(data);
+		// for(int i=0; i<3; i++)
+		// {
+		// 	cout<<i<<": "<<1.0f*data[i]*mpu9250.getGres()<<endl;
+		// }
+		// cout<<endl;
 		mpu9250.readMagData(data);
 		for(int i=0; i<3; i++)
 		{
