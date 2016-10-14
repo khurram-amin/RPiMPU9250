cdb14af338345649c9262e962dfc9b5b6b3910fc
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-14 23:07:33 +0300

    Commit, Fixed minor syntex error

--- main.cpp
+++ main.cpp
@@ -9,5 +9,25 @@ int main()
 
 	uint8_t c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
 
-	printf("%u", c);
+	printf("%u\n", c);
+
+	printf("MPU9250 is online...");
+
+    // Start by performing self test and reporting values
+    myIMU.MPU9250SelfTest(myIMU.SelfTest);
+    printf("x-axis self test: acceleration trim within : ");
+    printf(myIMU.SelfTest[0],1); printf("% of factory value\n");
+    printf("y-axis self test: acceleration trim within : ");
+	printf(myIMU.SelfTest[1],1); print("% of factory value\n");
+	printf("z-axis self test: acceleration trim within : ");
+	printf(myIMU.SelfTest[2],1); printf("% of factory value\n");
+	printf("x-axis self test: gyration trim within : ");
+	printf(myIMU.SelfTest[3],1); printf("% of factory value\n");
+	printf("y-axis self test: gyration trim within : ");
+	printf(myIMU.SelfTest[4],1); printf("% of factory value\n");
+	printf("z-axis self test: gyration trim within : ");
+	printf(myIMU.SelfTest[5],1); printf("% of factory value\n");	
+
+	//Calibrate gyro and accelerometers, load biases in bias registers
+    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
 }
