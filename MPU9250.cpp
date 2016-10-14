60d4f28c7dee6d87fe7e125ae4fcc36a5e699852
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-14 23:54:28 +0300

    Commit, Fixed minor syntex error

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -2,7 +2,7 @@
 
 MPU9250::MPU9250()
 {
-  wiringPiI2CSetup ((int)0x68); 
+  std::cout << wiringPiI2CSetup((int)0x68) << std::endl;
   // Specify sensor full scale
   Gscale = GFS_250DPS;
   Ascale = AFS_2G;
