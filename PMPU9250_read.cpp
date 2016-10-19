687779a5ce99d633d65695d39842be453a8a1a61
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-19 09:41:13 +0300

    Fourth Commit

--- PMPU9250_read.cpp
+++ PMPU9250_read.cpp
@@ -6,15 +6,16 @@ using namespace std;
 int main()
 {
 	short *data = new short[3];
+	
 	MPU9250 mpu9250;
-	cout<<"config mpu2950 ";
+	
 	mpu9250.initMPU9250();
-	cout<<"done!"<<endl;
+	
 	while(1)
 	{
 //		cout<<"reading data ..."<<endl;
-		mpu9250.readAccelData(data);
-		cout<<"T: "<<mpu9250.readTempInC()<<endl;
+		//mpu9250.readAccelData(data);
+		//cout<<"T: "<<mpu9250.readTempInC()<<endl;
 		// for(int i=0; i<3; i++)
 		// {
 		// 	cout<<i<<": "<<1.0f*data[i]*mpu9250.getAres()<<endl;
